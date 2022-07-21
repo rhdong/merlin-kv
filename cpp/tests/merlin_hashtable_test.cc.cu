cbec512f08c537adcd40f11011115ed18cfea370
rhdong <rhdong2017@gmail.com>
2022-07-21 11:41:27 +0800

    d4

--- cpp/tests/merlin_hashtable_test.cc.cu
+++ cpp/tests/merlin_hashtable_test.cc.cu
@@ -170,9 +170,10 @@ int test_main() {
       cur_load_factor = table_->load_factor();
 
       printf(
-          "[prepare] size=%zu, expect_size=%zu, insert_or_assign=%.2fms, find=%.2fms, "
+          "[prepare] size=%zu, expect_size=%zu, insert_or_assign=%.2fms, "
+          "find=%.2fms, "
           "cur_load_factor=%f\n",
-          table_->size(), start, diff_insert_or_assign.count() * 1000,
+          table_->size(), start + KEY_NUM, diff_insert_or_assign.count() * 1000,
           diff_find.count() * 1000, cur_load_factor);
 
       if (table_->size(stream) < start) {
