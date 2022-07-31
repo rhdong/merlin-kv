2c1e9be7955dce8738fa64396d0b52ad55b1b59b
rhdong <rhdong2017@gmail.com>
2022-07-31 12:37:58 +0800

    d3

--- cpp/include/merlin/core_kernels.cuh
+++ cpp/include/merlin/core_kernels.cuh
@@ -564,12 +564,12 @@ __global__ void upsert_kernel(const Table<K, V, M, DIM> *__restrict table,
 
     int key_pos = -1;
     bool found_or_empty = false;
-    const size_t bucket_max_size = table->bucket_max_size;
+    const unsigned int bucket_max_size = table->bucket_max_size;
     size_t key_idx = t / TILE_SIZE;
     K insert_key = keys[key_idx];
-    K hashed_key = Murmur3HashDevice(insert_key);
-    size_t bkt_idx = hashed_key & (table->buckets_num - 1);
-    size_t start_idx = hashed_key & (bucket_max_size - 1);
+    unsigned int hashed_key = Murmur3HashDevice(insert_key) & 0xFFFFFFFF;
+    unsigned int bkt_idx = hashed_key & (table->buckets_num - 1);
+    unsigned int start_idx = hashed_key & (bucket_max_size / TILE_SIZE - 1);
 
     Bucket<K, V, M, DIM> *bucket = table->buckets + bkt_idx;
 
@@ -579,9 +579,9 @@ __global__ void upsert_kernel(const Table<K, V, M, DIM> *__restrict table,
 //    g.sync();
 
 #pragma unroll
-    for (size_t tile_offset = 0; tile_offset < bucket_max_size;
+    for (unsigned int tile_offset = 0; tile_offset < bucket_max_size;
          tile_offset += TILE_SIZE) {
-      size_t key_offset = (start_idx + tile_offset + rank) % bucket_max_size;
+      unsigned int key_offset = (start_idx + tile_offset + rank) % bucket_max_size;
       K current_key = *(bucket->keys + key_offset);
       auto const found_or_empty_vote =
           g.ballot(key_empty<K>(&current_key) ||
