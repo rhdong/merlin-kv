216a86ff2064448229dfd3a162491ec4999cf5ff
rhdong <rhdong2017@gmail.com>
2022-08-02 10:04:00 +0800

    d1

--- cpp/tests/merlin_hashtable_test.cc.cu
+++ cpp/tests/merlin_hashtable_test.cc.cu
@@ -73,7 +73,7 @@ constexpr uint64_t MAX_SIZE = INIT_SIZE;
 constexpr uint64_t KEY_NUM = 1 * 1024 * 1024UL;
 constexpr uint64_t TEST_TIMES = 1;
 constexpr uint64_t DIM = 4;
-constexpr float target_load_factor = 0.007;
+constexpr float target_load_factor = 0.000;
 
 template <class K, class M>
 __forceinline__ __device__ bool erase_if_pred(const K &key, const M &meta) {
