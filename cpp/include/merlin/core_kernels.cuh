a22247985794715de9c5cec7d5e329af9bb769ab
rhdong <rhdong2017@gmail.com>
2022-08-02 17:45:13 +0800

    d3

--- cpp/include/merlin/core_kernels.cuh
+++ cpp/include/merlin/core_kernels.cuh
@@ -504,17 +504,17 @@ __global__ void upsert_kernel_with_io(
         key_pos = (start_idx + tile_offset + src_lane) & (bucket_max_size - 1);
         if (rank == src_lane) {
           bucket->keys[key_pos] = insert_key;
-//          bucket->metas[key_pos].val = metas[key_idx];
+          bucket->metas[key_pos].val = metas[key_idx];
           if (current_key == EMPTY_KEY) {
             buckets_size[bkt_idx]++;
             local_size++;
           }
         }
-//        local_size = g.shfl(local_size, src_lane);
-//        if (local_size >= bucket_max_size) {
-//          refresh_bucket_meta<K, V, M, DIM, TILE_SIZE>(g, bucket,
-//                                                       bucket_max_size);
-//        }
+        local_size = g.shfl(local_size, src_lane);
+        if (local_size >= bucket_max_size) {
+          refresh_bucket_meta<K, V, M, DIM, TILE_SIZE>(g, bucket,
+                                                       bucket_max_size);
+        }
         copy_vector<V, DIM, TILE_SIZE>(g, values + key_idx,
                                        bucket->vectors + key_pos);
         break;
